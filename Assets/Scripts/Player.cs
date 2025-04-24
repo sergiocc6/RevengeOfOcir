8d4b96c6a8e4f864d56714d450dfbf896c2f40c8
sergiocc6 <sergiocor6@gmail.com>
2025-04-24 12:42:45 +0200

    Background, wall slide, double jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,8 +9,16 @@ public class Player : MonoBehaviour
     public float moveSpeed = 7f;
     private bool facingRight = true;
 
-    public float jumpHeight = 10f;
+    public float jumpSpeed = 10f;
+    public float doubleJumpSpeed = 8f;
     public bool isGround = true;
+    bool canDoubleJump;
+
+    bool isTouchingFront = false;
+    bool wallSliding;
+    public float wallSlidingSpeed = 0.75f;
+    bool isTouchingWallRight;
+    bool isTouchingWallLeft;
 
     public Animator animator;
 
@@ -25,50 +33,88 @@ public class Player : MonoBehaviour
     {
         movement = Input.GetAxis("Horizontal");
 
-        if (movement < 0f && facingRight)
+        if (movement < 0f && facingRight && !isTouchingWallRight)
         {
             transform.eulerAngles = new Vector3(0f, -180f, 0f);
             facingRight = false;
         }
-        else if (movement > 0f && !facingRight)
+        else if (movement > 0f && !facingRight && !isTouchingWallLeft)
         {
             transform.eulerAngles = new Vector3(0f, 0f, 0f);
             facingRight = true;
         }
 
         //Salto
-        if (Input.GetKey(KeyCode.Space) && isGround)
+        if (Input.GetKey(KeyCode.Space))
         {
-            Jump();
-            isGround = false;
-            animator.SetBool("Jump", true);
-        } 
+            if (isGround)
+            {
+                animator.SetBool("Jump", true);
+                canDoubleJump = true;
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpSpeed);
+                isGround = false;
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    animator.SetBool("Jump", false);
+                    animator.SetBool("DoubleJump", true);
+                    rb.linearVelocity = new Vector2(rb.linearVelocity.x, doubleJumpSpeed);
+                    canDoubleJump = false;
+                    isGround = false;
+                }
+            }
+        }
 
-        //Animaciones
-        if(Math.Abs(movement) > .1f)
+        //Caer
+        if (rb.linearVelocity.y < 0f)
+        {
+            animator.SetBool("Jump", false);
+            if (!wallSliding)
+                animator.SetBool("Fall", true);
+        }
+        else if (rb.linearVelocity.y > 0f)
+        {
+            animator.SetBool("Fall", false);
+        }
+
+        //Correr
+        if (Math.Abs(movement) > .1f)
         {
             animator.SetFloat("Run", 1f);
         }
-        else if(movement < .1f)
+        else if (movement < .1f)
         {
             animator.SetFloat("Run", 0f);
         }
 
         //Ataque (click izquierdo)
-        if(Input.GetMouseButtonDown(0))
-        {            
+        if (Input.GetMouseButtonDown(0))
+        {
             animator.SetTrigger("Attack");
+        }
+
+        //Deslizarse pared
+        if (isTouchingFront && !isGround)
+        {
+            wallSliding = true;
+        }
+        else
+        {
+            wallSliding = false;
+        }
+
+        if (wallSliding)
+        {
+            animator.Play("Player_Wall");
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Clamp(rb.linearVelocity.y, -wallSlidingSpeed, float.MaxValue));
         }
     }
 
     private void FixedUpdate()
     {
         transform.position += new Vector3(movement, 0f, 0f) * Time.fixedDeltaTime * moveSpeed;
-    }
-
-    void Jump()
-    {
-        rb.AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -79,6 +125,30 @@ public class Player : MonoBehaviour
         {
             isGround = true;
             animator.SetBool("Jump", false);
+            animator.SetBool("DoubleJump", false);
+            animator.SetBool("Fall", false);
         }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "RightWall")
+        {
+            isTouchingFront = true;
+            isTouchingWallRight = true;
+        }
+
+        if (collision.gameObject.tag == "LeftWall")
+        {
+            isTouchingFront = true;
+            isTouchingWallLeft = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isTouchingFront = false;
+        isTouchingWallRight = false;
+        isTouchingWallLeft = false;
     }
 }
